9a389ba8ffe91e065ba55742155e8d7c4ddaebda
ilhambayhaqi <ilham.bhq28@gmail.com>
2021-06-10 15:38:25 +0700

    add account simple registration and login

--- client/client.py
+++ client/client.py
@@ -1,33 +1,90 @@
 import socket
 import sys
 import threading
+import os
+import pickle
 
 buff_size = 65535
 HOST = '127.0.0.1'
 PORT = 5000
 
-def read_message(socket_client):
-    while True:
-        data = socket_client.recv(buff_size)
-        if len(data) == 0:
-            break
-        print(data)
-
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_client.connect((HOST, PORT))
-print(sys.argv[1])
-socket_client.send(sys.argv[1].encode('utf-8'))
 
-thread_client = threading.Thread(target=read_message, args=(socket_client, ))
-thread_client.start()
+class Account:
+    def __init__ (self, id, name):
+        self.id = id
+        self.name = name
 
-while True:
-    dest = input("Username tujuan atau bcast untuk broadcast: \n")
-    message = input("Masukkan pesan: \n")
+def clear():
+    if os.name == 'nt':
+        _ = system('cls')
+    else:
+        _ = os.system('clear')
 
-    if message == 'exit':
-        socket_client.close()
-        break
+def header_page():
+    clear()
+    print('Welcome to Esaiyy Chat')
+    print('==============================')
+
+def register(status):
+    header_page()
+    print(status, end='')
+
+    id = input('Enter username: ')
+    name = input('Enter your name: ')
+    newAccount = Account(id, name)
+
+    registerRequest = pickle.dumps(('register', newAccount))
+    socket_client.send(registerRequest)
+    status = socket_client.recv(buff_size)
+    status = pickle.loads(status)
+
+    print(status)
+    if status == 'failed':
+        register('Account is already Exist\n')
+    else:
+        dasboard('Account Created, you\'re login now\n', status)
+    return
     
-    data = dest + '|' + message
-    socket_client.send(data.encode('utf-8'))
+def dasboard(status, myAccount):
+    header_page()
+    print(status, end='')
+    print('Hello, ' + myAccount.name)
+
+    socket_client.close()
+    return
+
+def login(status):
+    header_page()
+    print(status, end='')
+
+    id = input('Enter username: ')
+    loginRequest = pickle.dumps(('login', id))
+    socket_client.send(loginRequest)
+    status = socket_client.recv(buff_size)
+    status = pickle.loads(status)
+
+    print(status)
+    if status == 'failed':
+        login('Account is not Exist\n')
+    else:
+        dasboard('Login success\n', status)
+    return
+
+def welcome_page():
+    header_page()
+    status = ''
+    islogin = input('Have an account? (y/n) : ')
+    if islogin == 'n':
+        register(status)
+    elif islogin == 'y':
+        login(status)
+    else:
+        print('Byee!!')
+
+def main():  
+    welcome_page()
+
+if __name__ == '__main__':
+    main()
--- server/server.py
+++ server/server.py
@@ -1,53 +1,61 @@
 import socket
+import pickle
 import threading
+
+class AccountManager():
+    def __init__ (self):
+        self.account = {}
+        self.online = {}
+    def add_account(self, account):
+        check = self.account.get(account.id, None)
+        if check == None:
+            self.account[account.id] = account
+            return self.account[account.id]
+        else:
+            return b'failed'
+    def check_account(self, username):
+        return self.account.get(username, 'failed')
+
+
+class Account:
+    def __init__ (self, id, name):
+        self.id = id
+        self.name = name
+
+def read_message(socket_client, address_client):
+    while True:
+        request = socket_client.recv(buff_size)
+        if request:
+            print(request)
+            data = pickle.loads(request)
+
+            if data[0] == 'register' :
+                response = accountManager.add_account(data[1])
+                socket_client.send(pickle.dumps(response))
+
+            elif data[0] == 'login' :
+                response = accountManager.check_account(data[1])
+                socket_client.send(pickle.dumps(response))
+        else:
+            break
+    return
 
 buff_size = 65535
 HOST = '0.0.0.0'
 PORT = 5000
-
-def read_message(client, socket_client, address_client, username_client):
-    while True:
-        data = socket_client.recv(buff_size)
-        if len(data) == 0:
-            break
-
-        dest, message= data.decode('utf-8').split('|')
-        message = "<{}>: {}".format(username_client, message)
-
-        if dest == 'bcast':
-            send_broadcast(client, message, address_client)
-        else:
-            dest_socket_client = client[dest][0]
-            send_message(dest_socket_client, message)
-        print(data)
-    
-    socket_client.close()
-    print('Connection closed', address_client)
-
-def send_broadcast(clients, message, sender_address_client):
-    for socket_client, address_client, _ in clients.values():
-        if not (sender_address_client[0] == address_client[0] and sender_address_client[1] == address_client[1]):
-            send_message(socket_client, message)
-
-def send_message(socket_client, message):
-    socket_client.send(message.encode('utf-8'))
-
-socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_server.bind((HOST, PORT))
-socket_server.listen(5)
-
 clients = {}
+accountManager = AccountManager()
 
-try:
+def main():
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_server.bind((HOST, PORT))
+    socket_server.listen(5)
+    
     while True:
         socket_client, address_client = socket_server.accept()
 
-        username_client = socket_client.recv(buff_size).decode('utf-8')
-        print(username_client, 'joined')
-
-        thread_client = threading.Thread(target=read_message, args=(clients, socket_client, address_client, username_client))
+        thread_client = threading.Thread(target=read_message, args=(socket_client, address_client))
         thread_client.start()
 
-        clients[username_client] = (socket_client, address_client, thread_client)
-except KeyboardInterrupt:
-    socket_server.close()
+if __name__ == '__main__':
+    main()
